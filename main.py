fbe37b69f1b07a9fa6aec3d7522fb6411efd3a29
belyivadim <belyivadim01@gmail.com>
2022-01-01 20:39:33 +0200

    create client

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+import datetime
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
+import requests
+from requests.exceptions import HTTPError
+import json
 
 
-# Press the green button in the gutter to run the script.
+class MainWindow(QtWidgets.QMainWindow):
+    server_adress = 'http://localhost:5000'
+    message_id = 0
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('messanger.ui', self)
+        self.sendButton1.clicked.connect(self.send_button_clicked)
+
+    def send_button_clicked(self):
+        self.send_message()
+
+    def send_message(self):
+        user_name = self.userNameLE.text()
+        message_text = self.textLE.text()
+        time_stamp = str(datetime.datetime.today())
+        msg = f'{{"UserName": "{user_name}", "MessageText": "{message_text}", "TimeStamp": "{time_stamp}"}}'
+        print('Сообщение отправлено: ' + msg)
+        url = self.server_adress + '/api/Messanger'
+        data = json.loads(msg)  # string to json
+        r = requests.post(url, json=data)
+
+    def get_message(self, id):
+        url = self.server_adress + '/api/Messanger/' + str(id)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except HTTPError as http_err:
+            return None
+        except Exception as err:
+            return None
+        else:
+            text = response.text
+            return text
+
+    def timer_event(self):
+        msg = self.get_message(self.message_id)
+        while msg is not None:
+            msg = json.loads(msg)
+            user_name = msg['UserName']
+            message_text = msg['MessageText']
+            time_stamp = msg['TimeStamp']
+            msg_text = f'{time_stamp} <{user_name}>: {message_text}'
+            print(msg_text)
+            self.messagesWidget.insertItem(self.message_id, msg_text)
+            self.message_id += 1
+            msg = self.get_message(self.message_id)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    timer = QtCore.QTimer()
+    time = QtCore.QTime(0, 0, 0)
+    timer.timeout.connect(w.timer_event)
+    timer.start(5000)
+    sys.exit(app.exec())
